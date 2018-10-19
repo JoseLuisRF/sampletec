2287009d56e0ecef4c83aa6577f0be1ced7dff01
JoseLuisRF <luislang93@gmail.com>
2018-10-18 23:07:52 -0500

    Success basic unit test

---
+++ src/test/java/sample/tec/test/BasicExampleTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.tec.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import sample.tec.util.MessageUtil;
+
+/**
+ *
+ * @author kon5760
+ */
+public class BasicExampleTest {
+    
+   private String message = "Hello World";	
+   private MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {
+      assertEquals(message, messageUtil.printMessage());
+   }
+}
